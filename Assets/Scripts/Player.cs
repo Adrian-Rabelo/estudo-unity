4977593fe25910b24f919f976c194dae4934aa1d
Ádrian Rabelo <adrianmendes@setic.ro.gov.br>
2023-10-10 17:02:23 -0400

    extraindo trecho e configurando animação de pulo

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -28,28 +28,9 @@ public class Player : MonoBehaviour
     {
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
         transform.position += movement * Time.deltaTime * Speed;
-        // * Andando pra direita
-        if (Input.GetAxis("Horizontal") > 0f) 
-        {
-            transform.eulerAngles = new Vector3(0f, 0f, 0f);
-            anim.SetBool("walk", true);
-        }
-
-        // * Andando pra esquerda
-        if (Input.GetAxis("Horizontal") < 0f) 
-        {
-            transform.eulerAngles = new Vector3(0f, 180f, 0f);
-            anim.SetBool("walk", true);
-        }
-
-        // * Parado
-        if (Input.GetAxis("Horizontal") == 0f)
-        {
-            anim.SetBool("walk", false);
-        }
+        CheckDirections();
 
     }
-
     void Jump()
     {
         if (Input.GetButtonDown("Jump"))
@@ -58,6 +39,7 @@ public class Player : MonoBehaviour
             {
                 rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
                 doubleJump = true;
+                anim.SetBool("jump", true);
             }
             else
             {
@@ -70,11 +52,40 @@ public class Player : MonoBehaviour
         }
     }
 
+    /**
+     * Este método verifica quais as direções o jogador está se movendo
+     * para poder alterar a direção e a animação do sprite
+     */
+    private void CheckDirections()
+    {
+        // * Andando pra direita
+        if (Input.GetAxis("Horizontal") > 0f)
+        {
+            transform.eulerAngles = new Vector3(0f, 0f, 0f);
+            anim.SetBool("walk", true);
+        }
+
+
+        // * Andando pra esquerda
+        if (Input.GetAxis("Horizontal") < 0f)
+        {
+            transform.eulerAngles = new Vector3(0f, 180f, 0f);
+            anim.SetBool("walk", true);
+        }
+
+        // * Parado
+        if (Input.GetAxis("Horizontal") == 0f)
+        {
+            anim.SetBool("walk", false);
+        }
+    }
+
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.layer == 8)
         {
             isJumping = false;
+            anim.SetBool("jump", false);
         }
     }
 
